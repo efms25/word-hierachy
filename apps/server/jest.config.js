8c4efbe46f763925e9f6503537c476724215f283
Eric Santana <efms.king@gmail.com>
2024-10-28 17:46:11 -0300

    :test_tube: test(server): dict test for pass or exception

--- apps/server/jest.config.js
+++ apps/server/jest.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+export default {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    verbose: true,
 };
---
+++ apps/server/src/utils/dictLoader.spec.ts
@@ -0,0 +1,42 @@
+import fs from 'fs/promises'
+import { loadDict } from "./dictLoader";
+import * as hirarchyType from "../types/hirarchy.type";
+import { DICTS_PATH } from '../config/config';
+
+// jest.mock('fs/promises')
+
+describe('loadDict', () => {
+    const mockData = [
+      {
+        name: "Root",
+        children: [
+          {
+            name: "Child 1",
+            children: [],
+          },
+        ],
+      },
+    ];
+  
+    beforeEach(() => {
+      // Limpar todas as simulações antes de cada teste
+      jest.clearAllMocks();
+    });
+  
+    it('should read the JSON file and return the parsed data', async () => {
+      // Simula o retorno de fs.readFile
+      jest.spyOn(fs, 'readFile').mockResolvedValueOnce(JSON.stringify(mockData));
+  
+      const result = await loadDict();
+  
+      expect(fs.readFile).toHaveBeenCalledWith(DICTS_PATH, 'utf8'); 
+      expect(result).toEqual(mockData); 
+    });
+  
+    it('should throw an error if fs.readFile fails', async () => {
+      const errorMessage = 'Failed to read file';
+      jest.spyOn(fs, 'readFile').mockRejectedValueOnce(new Error(errorMessage));
+  
+      await expect(loadDict()).rejects.toThrow(errorMessage); 
+    });
+  });
