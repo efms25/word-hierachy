f38926a55590d9e3206199e4c356d56768fbc953
Eric Santana <efms.king@gmail.com>
2024-10-29 15:01:30 -0300

    :test_tube: test(server): metrics exception test

--- apps/server/src/utils/metrics.spec.ts
+++ apps/server/src/utils/metrics.spec.ts
@@ -1,14 +1,22 @@
 import { calculateTime } from "./metrics";
 
-describe('calculateTime', () => {
-    it('Should return the result of the callback function and its execution time', async () => {
-        const mockCallbackFunction = jest.fn(async () => {
-            return 2 + 2
-        })
+describe("calculateTime", () => {
+  it("Should return the result of the callback function and its execution time", async () => {
+    const mockCallbackFunction = jest.fn(async () => {
+      return 2 + 2;
+    });
 
-        const [result, time] = await calculateTime(mockCallbackFunction);
+    const [result, time] = await calculateTime(mockCallbackFunction);
 
-        expect(result).toBe(4);
-        expect(time).toBeGreaterThan(0);
+    expect(result).toBe(4);
+    expect(time).toBeGreaterThan(0);
+  });
+
+  it('should throw an error if the callback function also thorws', async () => {
+    const mockCallbackFunction = jest.fn(async () => {
+        throw new Error('Mock error');
     })
-})
+
+    await expect(calculateTime(mockCallbackFunction)).rejects.toThrow('Internal calculation error, please check the provided callback function. Error: Mock error')
+  })
+});
