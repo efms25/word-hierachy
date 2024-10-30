51eeef228ed2fbcdbca6324dbb4af322a31684f4
Eric Santana <efms.king@gmail.com>
2024-10-30 19:51:05 -0300

    :sparkles: feat(server): cli operations

--- apps/server/cli.ts
+++ apps/server/cli.ts
@@ -1,6 +1,9 @@
 import yargs from 'yargs';
+import Table from 'cli-table3'
 import { hideBin } from 'yargs/helpers'
 import { calculateTime } from "./src/utils/metrics";
+import { analyzeText } from './src/utils/hirarchyAnalyzer';
+import { loadDict } from './src/utils/dictLoader';
 
 async function loadParams() {
     const argv = yargs(hideBin(process.argv))
@@ -11,7 +14,7 @@ async function loadParams() {
                     description: 'Profundidade segundo a qual será analisada na hirarquia',
                     default: 1
                 })
-                .option('vervose', {
+                .option('verbose', {
                     type: 'boolean',
                     describe: "Retornar calculos de tempo de execução",
                     default: false
@@ -27,14 +30,42 @@ async function loadParams() {
 
 async function main() {
 
+    const heirarchyTree = await loadDict();
     const [args, argsLoadingTime] = await calculateTime(loadParams);
-    
-    console.log(args, 'args')
+    const [count, countTime] = await calculateTime(() => analyzeText(heirarchyTree,args._[1],args.depth))
+
     if(args.verbose) {
 
-        console.log('tempo de carregamento de parâmetros: ', argsLoadingTime)
+        const metricsTable = new Table({
+            head: ['Metrica', 'Tempo de execução'],
+            colWidths: [40,20]
+        })
+
+        metricsTable.push(
+            ['Tempo de carregamento de parâmetros: ', argsLoadingTime],
+            ['Tempo de verificação da frase: ', countTime]
+        )
+
+        console.log(metricsTable.toString());
     }
 
+    if(!count.size) {
+        console.log('Nenhuma palavra encontrada')
+        return;
+    }
+    
+    const resultTable = new Table({
+        head: ['Palavra de Referência', 'Mencionados'],
+        colWidths: [30, 20]
+    })
+
+
+    count.forEach((value: number, key: string) => {
+        resultTable.push(
+            [key, value]
+        )
+    })
+    console.log(resultTable.toString());
 }
 
 main();
