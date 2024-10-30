2f2e07c9c60c0e1ad17e48871b3547a5c2ecd458
Eric Santana <efms.king@gmail.com>
2024-10-30 19:11:50 -0300

    :books: docs(server): hirarchy analyzer comments

--- apps/server/src/utils/hirarchyAnalyzer.ts
+++ apps/server/src/utils/hirarchyAnalyzer.ts
@@ -1,4 +1,12 @@
+/**
+ * Função principal para analisar o texto com base em uma árvore hierárquica
+ * @Param tree - A árvore hierárquica a ser analisada
+ * @Param text - A frase a ser analisada
+ * @Param targetDepth - O nível de profundidade de interesse na hierarquia
+ * @returns hashMap com a contagem de palavras.
+ */
 import { Hierarchy } from "../types/hirarchy.type";
+
 
 export function analyzeText(
   tree: Array<Hierarchy>,
@@ -6,17 +14,28 @@ export function analyzeText(
   targetDepth: number
 ): Map<string,number>| null {
   const words: Array<string> = text.split(" ");
-  const counts = new Map();
-  let currentNameComparator: string;
+  // O hashMap possui um bom desepenho em sua execução,
+  // ideal para armazenar a contagem nos nós alvo
+  const counts = new Map();  
+  let currentNameComparator: string;  // Variável para armazenar o nome do nó no nível desejado
 
+  // Função recursiva para busca em profundidade na árvore
+  // seguindo a estratégia Depth first search, que foca em 
+  // explorar a profundidade da árvore antes de se mover para o nível
   function depthSearch(treeNode: Hierarchy, currentDepth: number = 1): void {
     if (currentDepth >= targetDepth) {
       if (currentDepth === targetDepth) {
         currentNameComparator = treeNode.name;
       }
+      // Verifica se o nome do nó é mencionado no texto
+      // pode-se ser utilizado outras estratégias de comparação,
+      // porém apenas utilizar a função includes encaixa-se bem,
+      // na regra de negócio requisitada no desafio
       const isWordMentioned = words.includes(treeNode.name.toLowerCase());
 
       if (isWordMentioned) {
+        // Incrementa a contagem se o nó já foi mencionado
+        // ou inicializa a contagem para um novo nó
         if (counts.has(currentNameComparator)) {
           counts.set(
             currentNameComparator,
@@ -28,6 +47,7 @@ export function analyzeText(
       }
     }
 
+    // Itera sobre os filhos do nó atual e chama a função recursivamente
     treeNode.children?.forEach((nd) => {
       depthSearch(nd, currentDepth + 1);
     });
